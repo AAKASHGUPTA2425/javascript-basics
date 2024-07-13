2f313d8f69cf7209df852a8b7c860533440689f0
AAKASHGUPTA2425 <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2024-07-13 08:37:33 +0000

    datatypes

---
+++ basics_01/datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3) we are using node js not browser
+
+
+console.log(3+3)
+console.log("aakash")
+
+let name="aakash"
+let age=19
+let isloggedIn = false
+
+//number= 2 to power 53
+//bigint
+//string=""
+//boolean=true/false
+//null=standalone value
+//undefined=
+//symbol= unique
+
+
+//object
+
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefine)
