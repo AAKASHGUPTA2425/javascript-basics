9c3ec08d9614e72431dee0ab2fb3dd51b6768bea
AAKASHGUPTA2425 <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2024-07-13 08:54:18 +0000

    conversion

---
+++ basics_01/conversionOperation.js
@@ -0,0 +1,10 @@
+let score="33"
+let scores=undefined
+console.log(typeof score)
+console.log(typeof (score))
+ let valueInNumber = Number(score)
+ let value = Number(scores)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+console.log(typeof value)
+console.log(value)
