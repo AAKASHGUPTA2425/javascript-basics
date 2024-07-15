5c7a6a61201879a76d7d525f836ffc11d688ddea
AAKASHGUPTA2425 <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2024-07-15 05:47:57 +0000

    number and maths

---
+++ basics_01/num_and_maths.js
@@ -0,0 +1,37 @@
+// const score=400
+// console.log(score);
+// const barrier= new Number(400)
+// console.log(barrier);
+// console.log(barrier.toString().length);
+// // to converting to string
+// console.log(barrier.toFixed(3));
+// // to adding digitS after point
+
+
+
+// const num=123.897
+// console.log(num.toPrecision(3));
+
+// const zeroes=100000000
+// console.log(zeroes.toLocaleString('en-IN'));
+// console.log(zeroes.toLocaleString());
+
+
+// +++++++++++++++++++++++++++Maths+++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(4))
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.3))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+
+// console.log(Math.min(3,2,54,32,1,0));
+// console.log(Math.max(3,2,54,32,1,0));
+console.log(Math.random());//random is range of 0 to 1
+console.log(Math.floor(Math.random() * 10) + 1);//this will range from 1 to 9
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)//this will range from given range
