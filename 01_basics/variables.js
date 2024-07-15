c2148d04ad1da5e07affe3d3fe453b54ed75dfe3
AAKASHGUPTA2425 <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2024-07-15 17:17:47 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 14423
+let accountemail = "aakasj@gmail.com"
+var accountpassward = "121212121"
+accountcity = "delhi"
+let accountstate
+accountemail="aakash@gmail.com"
+accountpassward="23232323"
+accountcity="chennai"
+console.table([accountId,accountemail,accountpassward,accountstate,accountcity])
