79811e842dcbfb8c49cf129a5e24aa7448a7dc20
AAKASHGUPTA2425 <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2024-07-15 17:16:23 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,36 @@
+const arr1 = [0, 1, 2, 3, 4]
+const arr3 = ["aakash", "harsh", "sid"]
+const arr2 = new Array(1, 2, 3, 4, 5)
+// console.log(arr1[0]);
+// console.log(arr2[1]);
+// console.log(arr3[0]);
+// arr1.push(9)
+// arr1.push(7)
+// console.log(arr1);
+// arr1.pop()
+// console.log(arr1);
+// arr1.unshift(9)
+// console.log(arr1);
+// arr1.shift()
+// console.log(arr1);
+// arr1.shift()
+// console.log(arr1);
+// console.log(arr1.includes(9));
+// console.log(arr1.indexOf(9));
+// console.log(arr1.indexOf(3));
+
+// const arr4=arr1.join()
+// console.log(arr4);
+// console.log(arr1);
+// console.log(typeof arr4);
+// console.log(typeof arr1);
+
+
+//slice/splice
+console.log("A",arr1)
+const arr5=arr1.slice(1,3)
+console.log(arr5);
+console.log("B",arr1);
+const arr6=arr1.splice(1,3)
+console.log(arr6);
+console.log("C",arr1);
