bbd033a33a1f12e6676f0de7db4319dd5623db2c
AAKASHGUPTA2425 <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2024-07-15 06:03:34 +0000

    date and time

---
+++ basics_01/Date_and_time.js
@@ -0,0 +1,28 @@
+//DATES
+
+// let mydate=new Date()
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleTimeString());
+
+// let mydate=new Date(2023,0,30)
+// let mydate = new Date(2023, 0, 30, 5, 3)
+// console.log(mydate.toISOString());
+// let mydate = new Date("2023-01-14")
+let mydate = new Date("01-14-2023")
+// console.log(mydate.toLocaleString());
+let mytime= Date.now()
+console.log(mytime);
+console.log(mydate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let days=new Date()
+console.log(days.getDay())
+console.log(days.getMonth())
+console.log(days.getFullYear())
+console.log(days.getTime())
+
