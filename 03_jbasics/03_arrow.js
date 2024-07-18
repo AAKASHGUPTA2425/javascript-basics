2e9c6407e9c31955e46a54520d5c52418a87515f
AAKASHGUPTA2425 <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2024-07-18 17:25:42 +0000

    arrow

---
+++ 03_jbasics/03_arrow.js
@@ -0,0 +1,36 @@
+const user={
+    username:"aakash",
+    price:999,
+  welcome:function(username){
+    console.log(`${this.username},welcome to code`);
+    // console.log(this);
+  }
+
+}
+// user.welcome()
+// user.username="sam"
+// user.welcome()
+// console.log(this);//at window it showes window because of multiple objects
+
+// function chai() {
+//   let  username="aakash"
+//     console.log(this.username);
+// }
+// chai()
+
+const chai=function(){
+    let  username="aakash"
+        console.log(this);
+}
+// chai()
+
+// const chai=()=>{
+//     let username="aakash"
+//     console.log(this);
+// }
+// chai()
+
+const arrowfunction =(num1,num2) => {
+  return num1+num2
+}
+console.log(arrowfunction(3,4));
