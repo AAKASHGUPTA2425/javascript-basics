3248de608fc2d0ffca50ab620416dff0b17b5bdf
AAKASHGUPTA2425 <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2024-07-19 17:51:48 +0000

    while loop

---
+++ 05_iterations/02_whileloop.js
@@ -0,0 +1,19 @@
+let index=0
+while (index<11) {
+    // console.log(index);
+    index++
+}
+
+let arr=[1,2,3,4,5]
+const i=arr.length
+let j=0
+while (j<i) {
+    // console.log(arr[j]);
+    j++
+}
+let scope=11
+do {
+    console.log(scope);
+    scope=scope+1
+    
+} while (scope<=10);
