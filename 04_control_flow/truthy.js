00e2be4765a76c9db2616d9807d26045c69b1b11
AAKASHGUPTA2425 <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2024-07-19 12:58:39 +0000

    trthy/falsy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,50 @@
+const userid="aaka@gmail.com"
+const user=""
+const arr=[]
+// if(userid){
+//     console.log("user has id");
+// }else{
+//     console.log("user is not active");
+// }
+// if(user){
+//     console.log("user is active");
+// }else{
+//     console.log("user is not active");
+// }
+
+// if(arr){
+//     console.log("arr is active");
+// }else{
+//     console.log("arr is not active");
+// }
+
+
+//falsy values
+// false,0,-0,BigInt 0n,"",null,undefined,null(NaN)
+//truthy values
+//"0",'false'," ",[],{},function(){}
+
+// if(arr.length===0){
+//     console.log("array is empty");
+// }
+
+// const emptyObject ={}
+// if(Object.keys(emptyObject).length===0){
+//     console.log("object is empty");
+// }
+
+
+
+//Nullish Coalescing Operator (??):null undefined
+let val1;
+// val1=5??10
+// val1=null??10
+// val1=undefined??10
+// val1=null??10??15
+
+
+// console.log(val1);
+//terniary operator
+//condition ?true:false
+const iceteaprice=100
+iceteaprice<=100 ?console.log("less then or equal to 100"):console.log("more then 100")
