3bd7a02cc9125d8ddc29e4aff857ca035f480fbe
AAKASHGUPTA2425 <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2024-07-19 12:58:16 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,19 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+switch (month=3) {
+    case 1:
+        console.log("januery");
+        break;
+    case 2:
+        console.log("februrary");
+    case 3:
+        console.log("march");   
+    default:
+        break;
+}
