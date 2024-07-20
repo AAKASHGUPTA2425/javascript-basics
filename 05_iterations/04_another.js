3f5463dc8972e89df7e7ec1828944efda684dcc4
AAKASHGUPTA2425 <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2024-07-20 11:27:51 +0000

    for in

---
+++ 05_iterations/04_another.js
@@ -0,0 +1,22 @@
+const myobj={
+    js:"javascript",
+    py:"python",
+    cpp:"c++"
+}
+for (const key in myobj) {
+    
+        // console.log(`${key} stands for ${myobj[key]}`);
+    
+}
+const language=["c++","python","java","javascript"]
+for (const key in language) {
+    // console.log(`${key} and ${language[key]}`);
+}
+
+// const map= new Map
+// map.set('eng','english')
+// map.set('hin','hindi')
+// map.set('ssc','social studies')
+// for (const key in map) {
+//    console.log(`${key} is stands for ${map[key]}`);
+// }
