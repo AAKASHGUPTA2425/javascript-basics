e2007d1a5e63b54945d89e23e5e7a0acb24e80ec
AAKASHGUPTA2425 <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2024-07-27 06:36:35 +0000

    color change

--- 07_projects/01_color_change/script.js
+++ 07_projects/01_color_change/script.js
@@ -5,17 +5,34 @@ buttons.forEach(function (button) {
     button.addEventListener('click', function (e) {
         console.log(e)
         console.log(e.target);
-if (e.target.id==="grey") {
-    body.style.backgroundColor="grey"
-}
-if (e.target.id==="white") {
-    body.style.backgroundColor="white"
-}
-if (e.target.id==="blue") {
-    body.style.backgroundColor="blue"
-}
-if (e.target.id==="yellow") {
-    body.style.backgroundColor="yellow"
-}
+        // if (e.target.id==="grey") {
+        //     body.style.backgroundColor="grey"
+        // }
+        // if (e.target.id==="white") {
+        //     body.style.backgroundColor="white"
+        // }
+        // if (e.target.id==="blue") {
+        //     body.style.backgroundColor="blue"
+        // }
+        // if (e.target.id==="yellow") {
+        //     body.style.backgroundColor="yellow"
+        // }
+        switch (e.target.id) {
+            case 'grey':
+                body.style.backgroundColor = "grey"
+                break;
+            case 'white':
+                body.style.backgroundColor = "white"
+                break;
+            case 'blue':
+                body.style.backgroundColor = "blue"
+                break;
+            case 'yellow':
+                body.style.backgroundColor = "yellow"
+                break;
+
+            default:
+                break;
+        }
     })
 })
