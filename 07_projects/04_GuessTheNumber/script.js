e271b364f60154dbc5496ac433b46092fa749586
AAKASHGUPTA2425 <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2024-07-29 17:09:05 +0000

    Number_Game

---
+++ 07_projects/04_GuessTheNumber/script.js
@@ -0,0 +1,29 @@
+const ran=console.log(Math.floor((Math.random()*100)+1))
+const submit=document.querySelector('#subt')
+const uservalue=document.querySelector('#guessField')
+const Prevguesses=document.querySelector('.guesses')
+const remainingguesses=document.querySelector('.lastResult')
+const lowOrHi=document.querySelector('.lowOrHi')
+const startover=document.querySelector('.resultParas')
+const p=document.createElement('p')
+let prevguess=[]
+let numguess=1;
+let playgame=true;
+function validguess(guess) {
+    //
+}
+function checkguess(guess) {
+    //
+}
+function displayguess(guess) {
+    //
+}
+function displaymessage(guess) {
+    //
+}
+function newgame() {
+    //
+}
+function endgame() {
+    //
+}
