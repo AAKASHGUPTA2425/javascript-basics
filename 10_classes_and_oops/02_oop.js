660d6c5b2f15ccae9a74e52047512a416bc42d90
AAKASHGUPTA2425 <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2024-08-13 10:47:48 +0000

    oops

--- 10_classes_and_oops/02_oop.js
+++ 10_classes_and_oops/02_oop.js
@@ -3,9 +3,26 @@ const user ={
     loginCount:8,
     signeIn:true,
     getuserdetails:function(){
-       console.log("Got user detail from database");
+    //    console.log("Got user detail from database");
+    // console.log(`username:${this.username}`);
+    console.log(this);
+    
+    
     }
 
 }
-console.log(user.username);
-console.log(user.getuserdetails());
+// console.log(user.username);
+// console.log(user.getuserdetails());
+// console.log(this);
+function User (username,loginCount,signeIn){
+    this.username=username;
+    this.loginCount=loginCount;
+    this.signeIn=signeIn
+
+    return this
+}
+const userOne=new User('aakash',23,true);
+const usertwo=new User('harsh',23,false);
+console.log(userOne);
+console.log(usertwo);
+
